82f7ecdec59d458294a5b1b5292c5b69354dcb13
江灏 <493473098@qq.com>
2020-07-25 19:39:51 +0800

    完成了个人中心的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,14 +2,16 @@
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
-// 载发起ajax前拼接请求的跟路径
-options.url='http://ajax.frontend.itheima.net'+ options.url
- // 统一为有权限的接口，设置 headers 请求头
- if (options.url.indexOf('/my/') !== -1) {
+  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+  options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+  // 统一为有权限的接口，设置 headers 请求头
+  if (options.url.indexOf('/my/') !== -1) {
     options.headers = {
       Authorization: localStorage.getItem('token') || ''
     }
   }
+
   // 全局统一挂载 complete 回调函数
   options.complete = function(res) {
     // console.log('执行了 complete 回调：')
--- assets/js/login.js
+++ assets/js/login.js
@@ -44,52 +44,31 @@ $(function() {
       if (res.status !== 0) {
         return layer.msg(res.message)
       }
-   layer.msg('注册成功，请登录！')
+      layer.msg('注册成功，请登录！')
       // 模拟人的点击行为
       $('#link_login').click()
     })
   })
+
   // 监听登录表单的提交事件
-  $('#form_login').submit(function(e){
-    // 阻止默认交行为
+  $('#form_login').submit(function(e) {
+    // 阻止默认提交行为
     e.preventDefault()
     $.ajax({
-      url:'/api/login',
-      method:'POST',
-      data:$(this).serialize(),
-      success:function(res){
-        if(res.status !==0) {
-        return layer.msg('登陆失败')
+      url: '/api/login',
+      method: 'POST',
+      // 快速获取表单中的数据
+      data: $(this).serialize(),
+      success: function(res) {
+        if (res.status !== 0) {
+          return layer.msg('登录失败！')
         }
-           layer.msg('登陆成功')
-           localStorage.setItem('token',res.token)
-           location.href='/index.html'
+        layer.msg('登录成功！')
+        // 将登录成功得到的 token 字符串，保存到 localStorage 中
+        localStorage.setItem('token', res.token)
+        // 跳转到后台主页
+        location.href = '/index.html'
       }
-
     })
   })
-
-  
-
-  // // 监听登录表单的提交事件
-  // $('#form_login').submit(function(e) {
-  //   // 阻止默认提交行为
-  //   e.preventDefault()
-  //   $.ajax({
-  //     url: '/api/login',
-  //     method: 'POST',
-  //     // 快速获取表单中的数据
-  //     data: $(this).serialize(),
-  //     success: function(res) {
-  //       if (res.status !== 0) {
-  //         return layer.msg('登录失败！')
-  //       }
-  //       layer.msg('登录成功！')
-  //       // 将登录成功得到的 token 字符串，保存到 localStorage 中
-  //       localStorage.setItem('token', res.token)
-  //       // 跳转到后台主页
-  //       location.href = '/index.html'
-  //     }
-  //   })
-  // })
 })
